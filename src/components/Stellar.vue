782a95cc9c53320bc5898d5c49f2fea73f4b4e71
Jérémy <rcd.jeremy@gmail.com>
2021-06-09 21:41:24 +0200

    Camera updates and raycaster
    TD: Appearing pages

--- src/components/Stellar.vue
+++ src/components/Stellar.vue
@@ -5,28 +5,42 @@
 
 <script>
 import * as THREE from 'three';
-
-
 import Stats from 'stats-js';
+import { OrbitControls } from './OrbitControl.js';
+
+const CAMERA_DIST = 3000
+
+var controls;
 
 let SCREEN_WIDTH = window.innerWidth;
 let SCREEN_HEIGHT = window.innerHeight;
 let aspect = SCREEN_WIDTH / SCREEN_HEIGHT;
+let mouseX = 0, mouseY = 0, clicked = false;
 
 let container, stats;
-let camera, scene, renderer, activeCamera;
+let camera, scene, renderer, activeCamera, cameraDebug;
 let activeStar, toStar = null, count=0;
 let extoStar;
 
-let starTab = [];
+let starTab = [], meshTab = [];
 let sun, starPassion, starSkill, starDegree, starCamera;
 let satelliteTab = [];
 let satelliteSkillCampus;
 
 let cameraBis, cameraTierce;
-let cameraBisHelper, cameraTierceHelper;
+let cameraHelper, cameraBisHelper, cameraTierceHelper;
 const frustumSize = 600;
 
+var rotWorldMatrix, rotObjectMatrix;
+
+/* Star object 
+  dist = distance with the sun
+  height = size of the star
+  clr = color
+  ..
+  ..
+  axe = axe parameter, maybe useful later
+  orbiteWith = For satellite, link to the star*/
 class Star {
   constructor( parameters) {
     this.dist=0;
@@ -34,29 +48,42 @@ class Star {
     this.clr=0xffffff;
     this.rotationSpeed=0;
     this.orbitalSpeed=1;
-    this.axe=0;
+    this.axis = new THREE.Vector3(0,1,0);
     this.orbiteWith=null,
 
     this.setValues( parameters );
 
     this.mesh=new THREE.Mesh(
       new THREE.SphereGeometry( this.height, 8, 8 ),
-      new THREE.MeshBasicMaterial( { color: this.clr, wireframe: true } )
+      new THREE.MeshBasicMaterial( { color: this.clr, wireframe: false } )
     );
 
-    if (this.orbiteWith) {
-      this.orbiteWith.mesh.add( this.mesh )
-    }
+    // this.mesh.geometry.computeBoundingSphere();
 
+
+    this.orbit = new THREE.Line(
+      new THREE.EdgesGeometry(
+        new THREE.CircleGeometry(this.dist, 90).rotateX(Math.PI/2)),
+        new THREE.MeshBasicMaterial({
+          color: this.orbiteWith ? 0x00ff00 : 0xff0000,
+          transparent: true,
+          opacity: .5,
+          side: THREE.BackSide})
+    );
   }
 
   orbite(r) {
-    this.mesh.position.x = this.dist* Math.cos( r * this.orbitalSpeed);
-    this.mesh.position.y = this.dist* Math.sin( r * this.orbitalSpeed);  
+    if (this.orbiteWith) {
+      this.mesh.position.z = this.orbiteWith.mesh.position.z + this.dist* Math.cos( r * this.orbitalSpeed)
+      this.mesh.position.x = this.orbiteWith.mesh.position.x + this.dist* Math.sin( r * this.orbitalSpeed)
+    } else {
+      this.mesh.position.z = this.dist* Math.cos( r * this.orbitalSpeed);
+      this.mesh.position.x = this.dist* Math.sin( r * this.orbitalSpeed); 
+    }
   }
 
   rotate(r) {
-    this.mesh.rotation.z = Math.PI / 2 * r * this.rotationSpeed;
+    rotateAroundObjectAxis(this.mesh, this.axis, (r%360)*Math.PI/36000*this.rotationSpeed)
   }
 
   update(r) {
@@ -101,22 +128,85 @@ class Star {
 	}
 };
 
+scene = new THREE.Scene();
+const axesHelper = new THREE.AxesHelper( 1000 );
+scene.add( axesHelper );
 
-init();
+container = document.createElement( 'div' );
+document.body.appendChild( container );
+
+renderer = new THREE.WebGLRenderer( { antialias: true } );
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( SCREEN_WIDTH, SCREEN_HEIGHT );
+container.appendChild( renderer.domElement );
+renderer.autoClear = false;
+
+stats = new Stats();
+container.appendChild( stats.dom );
+
+camera = new THREE.PerspectiveCamera( 50, aspect, 45, 50000 );
+controls = new OrbitControls( camera, renderer.domElement );
+// controls.autoRotate = true;
+
+starInit();
+backgroundInit();
+cameraInit();
 animate();
 
-function init() {
+activeCamera = camera;
+cameraDebug.lookAt( sun.mesh.position )
 
-  container = document.createElement( 'div' );
-  document.body.appendChild( container );
+camera.lookAt( sun.mesh.position )
 
-  scene = new THREE.Scene();
+let raycaster = new THREE.Raycaster();
+let mouse = new THREE.Vector2();
 
+// Add event listeners
+document.addEventListener("pointerdown", onPointerDown, false);
+// document.addEventListener("pointermove", onPointerHover, false);
+document.addEventListener("pointermove", onPointerMove, false);
+// document.addEventListener("pointerup", onPointerUp, false);
+window.addEventListener( 'resize', onWindowResize );
+
+function onKeyDown( event ) {
+  console.log('Key Down');
+}
+
+function onPointerDown( event ) {
+  clicked = true
+  document.addEventListener("pointerup", onPointerUp, false);
+}
+
+function onPointerUp ( event ) {
+  clicked = false;
+  document.removeEventListener('pointerup', onPointerUp, false);
+
+  raycaster.setFromCamera( mouse, activeCamera );
+    const intersects = raycaster.intersectObjects( meshTab );
+    if ( intersects.length > 0 ) {
+      meshclicked(intersects[0].object)
+    }
+  
+}
+
+function onPointerMove( event ) {
+	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+}
+
+function onPointerHover( event )
+{
+  //
+}
+
+function cameraInit() {
   // Adding camera
+  camera.position.z = CAMERA_DIST*Math.cos(25 * (Math.PI/180));
+  camera.position.y = CAMERA_DIST*Math.sin(25 * (Math.PI/180));  
 
-  camera = new THREE.PerspectiveCamera( 50, aspect, 1, 10000 );
-  camera.position.z = 1000;
-  camera.position.y = -1500;
+  cameraDebug = new THREE.PerspectiveCamera( 50, aspect, 1, 10000 );
+  cameraDebug.position.z = CAMERA_DIST*2*Math.cos(45 * (Math.PI/180));
+  // cameraDebug.position.y = CAMERA_DIST*2*Math.sin(45 * (Math.PI/180));
 
   cameraBis = new THREE.PerspectiveCamera( 50, aspect, 150, 1000 );
   cameraBis.position.z = 500;
@@ -128,21 +218,26 @@ function init() {
 
   scene.add( cameraBis );
 
-  cameraBisHelper = new THREE.CameraHelper( cameraBis );
-  scene.add( cameraBisHelper );
+  cameraHelper = new THREE.CameraHelper( camera );
+  scene.add( cameraHelper );
 
-  cameraTierceHelper = new THREE.CameraHelper( cameraTierce );
-  scene.add( cameraTierceHelper );
+  // cameraBisHelper = new THREE.CameraHelper( cameraBis );
+  // scene.add( cameraBisHelper );
 
+  // cameraTierceHelper = new THREE.CameraHelper( cameraTierce );
+  // scene.add( cameraTierceHelper );
+}
+
+function starInit() {
   // Adding some stars
-
   starTab.push(
     sun = new Star({height: 100, clr:0xff0000, rotationSpeed: -1}), 
     starSkill = new Star({dist:500, clr:0x00ff00, orbitalSpeed: 0.7}), 
-    starPassion = new Star({dist:1000, orbitalSpeed:0.2}), 
+    starPassion = new Star({dist:1000, orbitalSpeed:0.2, rotationSpeed: -1, axis: new THREE.Vector3(1,1,0)}), 
     starDegree = new Star({dist:1500, orbitalSpeed:0.1}),
-    
+    satelliteSkillCampus = new Star({height: 10, dist:50, orbitalSpeed:30,orbiteWith: starSkill}),
   )
+  
   starCamera = new Star({orbitalSpeed: 0, rotationSpeed:0})
   // scene.add( starCamera.mesh )
 
@@ -152,7 +247,7 @@ function init() {
   //Adding some satellite
 
   satelliteTab.push(
-    satelliteSkillCampus = new Star({height: 10, dist:50, orbitalSpeed:30, orbiteWith: starSkill}),
+    
   )
 
   console.log(starTab)
@@ -161,47 +256,34 @@ function init() {
 
   starTab.forEach(star => {
       scene.add( star.mesh );
+      meshTab.push(star.mesh)
+      // star.orbit.geometry.vertices.shift();
+      // star.orbit.rotation.x = THREE.Math.degToRad(90);
+      if (star.orbiteWith) {
+        star.orbiteWith.mesh.add(star.orbit)
+      } else {
+        scene.add( star.orbit );
+      }
     }
   );
-  
+}
 
-  //
+function backgroundInit() {
+  // const geometry = new THREE.BufferGeometry();
+  // const vertices = [];
 
-  const geometry = new THREE.BufferGeometry();
-  const vertices = [];
+  // for ( let i = 0; i < 10000; i ++ ) {
 
-  for ( let i = 0; i < 10000; i ++ ) {
+  //   vertices.push( THREE.MathUtils.randFloatSpread( 2000 ) ); // x
+  //   vertices.push( THREE.MathUtils.randFloatSpread( 2000 ) ); // y
+  //   vertices.push( THREE.MathUtils.randFloatSpread( 2000 ) ); // z
 
-    vertices.push( THREE.MathUtils.randFloatSpread( 2000 ) ); // x
-    vertices.push( THREE.MathUtils.randFloatSpread( 2000 ) ); // y
-    vertices.push( THREE.MathUtils.randFloatSpread( 2000 ) ); // z
+  // }
 
-  }
+  // geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( vertices, 3 ) );
 
-  geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( vertices, 3 ) );
-
-  const particles = new THREE.Points( geometry, new THREE.PointsMaterial( { color: 0x888888 } ) );
-  scene.add( particles );
-
-  //
-
-  renderer = new THREE.WebGLRenderer( { antialias: true } );
-  renderer.setPixelRatio( window.devicePixelRatio );
-  renderer.setSize( SCREEN_WIDTH, SCREEN_HEIGHT );
-  container.appendChild( renderer.domElement );
-
-  renderer.autoClear = false;
-
-  //
-
-  stats = new Stats();
-  container.appendChild( stats.dom );
-
-  //
-
-  window.addEventListener( 'resize', onWindowResize );
-  document.addEventListener( 'keydown', onKeyDown );
-
+  // const particles = new THREE.Points( geometry, new THREE.PointsMaterial( { color: 0x888888 } ) );
+  // scene.add( particles );
 }
 
 /* This function return a speed with the distance parameter given for the transition function*/
@@ -235,45 +317,26 @@ function Transition(camera, toStar, r) {
     camera.position.x = 0
     camera.position.y = 0
     extoStar = null;
+    activeCamera = cameraTierce
     return 0;
   }
 }
 
-
-function onKeyDown( event ) {
-
-  switch ( event.keyCode ) {
-
-    case 79: /*O*/
-      if (!toStar) {
-        count = (count+ 1)%starTab.length
-        activeStar = starTab[count]
-      } else {
-        console.log('Cant beacause traveling')
-      }
-      break;
-
-    case 80: /*P*/
-      count = (count+ 1)%starTab.length
-      toStar = starTab[count] == activeStar ? null : starTab[count];
-      if (activeCamera == cameraTierce) {
-        cameraBis.position.x = activeStar.mesh.position.x;
-        cameraBis.position.y = activeStar.mesh.position.y;
-      }
-      activeCamera = cameraBis;
-      //(null if toStar[count] = actualStar else starTab[count]);
-      // cameraTransition(camera, activeStar, starTab[(count+ 1)%starTab.length])
-      // activeHelper = cameraBisHelper;
-      break;
-    
-    case 81: /*Q*/
-      starCamera.mesh.position.z = 250;
-      break;
-  }
-
+function rotateAroundObjectAxis(object, axis, radians) {
+    rotObjectMatrix = new THREE.Matrix4();
+    rotObjectMatrix.makeRotationAxis(axis.normalize(), radians);
+    object.matrix.multiply(rotObjectMatrix);
+    object.rotation.setFromRotationMatrix(object.matrix);
 }
 
-//
+// Rotate an object around an arbitrary axis in world space       
+function rotateAroundWorldAxis(object, axis, radians) {
+    rotWorldMatrix = new THREE.Matrix4();
+    rotWorldMatrix.makeRotationAxis(axis.normalize(), radians);
+    rotWorldMatrix.multiply(object.matrix);
+    object.matrix = rotWorldMatrix;
+    object.rotation.setFromRotationMatrix(object.matrix);
+}
 
 function onWindowResize() {
 
@@ -290,18 +353,33 @@ function onWindowResize() {
   cameraBis.updateProjectionMatrix();
 }
 
+function meshclicked( mesh ) {
+  switch (mesh) {
+    case sun.mesh:
+      console.log('Sun mesh')
+      
+      break;
+    case starSkill.mesh:
+      console.log("Star skill")
+
+      break;
+      
+    default:
+      console.log("Star unaccepted")
+      break;
+  }
+}
 //
 
 function animate() {
 
   requestAnimationFrame( animate );
 
+  controls.update()
   render();
   stats.update();
 
 }
-
-
 
 function render() {
 
@@ -322,18 +400,14 @@ function render() {
     activeStar = toStar ? toStar : activeStar
     toStar = null;
     extoStar = null;
-    activeCamera = cameraTierce;
   }
 
-  // cameraBis.lookAt( sun.mesh.position );
-  renderer.clear();
 
+  renderer.clear();
   renderer.setViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT );
   renderer.render( scene, activeCamera );
-
-  camera.lookAt( sun.mesh.position );
-  renderer.setViewport( 0, 0, SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4);
-  renderer.render( scene, camera );
+  renderer.setViewport( 0, 0, SCREEN_WIDTH / 3.5, SCREEN_HEIGHT / 3.5);
+  renderer.render( scene, cameraDebug );
 }
 
 export default {
